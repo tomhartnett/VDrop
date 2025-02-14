f957194e621d7699c40f6fc749e8d3a127b8a544
Tom Hartnett <tomhartnett@gmail.com>
2025-02-14 07:29:23 -0600

    Initial Commit

---
+++ VDrop/VDropApp.swift
@@ -0,0 +1,17 @@
+//
+//  VDropApp.swift
+//  VDrop
+//
+//  Created by Tom Hartnett on 2/14/25.
+//
+
+import SwiftUI
+
+@main
+struct VDropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
