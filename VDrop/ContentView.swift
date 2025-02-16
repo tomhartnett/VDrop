62ecc7ff8bf0b58a1af642756eb2b7cd52b4102b
Tom Hartnett <tomhartnett@gmail.com>
2025-02-15 18:26:23 -0600

    Layout tweaks.

--- VDrop/ContentView.swift
+++ VDrop/ContentView.swift
@@ -18,7 +18,7 @@ struct ContentView: View {
     @State private var viewModel = ViewModel()
 
     var body: some View {
-        VStack(spacing: 24) {
+        VStack(spacing: 16) {
             VStack(spacing: 16) {
                 if viewModel.isWorking {
                     ProgressView()
@@ -32,15 +32,15 @@ struct ContentView: View {
 
                     Text("Drop file here")
                         .font(.title2)
-                        .foregroundStyle(.secondary)
+                        .foregroundStyle(isHover ? .primary : .secondary)
                 }
             }
             .frame(width: 250, height: 125)
             .cornerRadius(12)
-            .background(isHover ? Color(nsColor: .white) : Color(nsColor: .windowBackgroundColor))
             .overlay(
                 RoundedRectangle(cornerRadius: 12)
-                    .stroke(Color.green, lineWidth: 2)
+                    .stroke(Color.green.opacity(isHover || viewModel.isWorking ? 1 : 0.5),
+                            lineWidth: isHover ? 4 : 2)
             )
             .onDrop(of: viewModel.supportedTypes, isTargeted: $isHover) { providers in
                 guard let provider = providers.first,
@@ -57,16 +57,16 @@ struct ContentView: View {
                 return true
             }
 
-            VStack(alignment: .leading) {
+            VStack(alignment: .leading, spacing: 16) {
                 Toggle(isOn: $isDownscaleEnabled) {
                     Text("Downscale video")
                 }
 
-                Picker("", selection: $selectedPreset) {
+                Picker(selection: $selectedPreset, content: {
                     ForEach(DownscalePreset.devicePresets) { preset in
                         Text(preset.name).tag(preset)
                     }
-                }
+                }, label: { EmptyView() })
                 .frame(width: 250)
                 .disabled(!isDownscaleEnabled)
             }
